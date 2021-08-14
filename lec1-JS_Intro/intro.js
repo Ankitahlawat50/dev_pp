3fd92b8acade68955b7b3854c0dda703a82a722b
Ankit Ahlawat <ankitahlawat.50@gamil.com>
2021-08-14 21:20:22 +0530

    js intro

---
+++ lec1-JS_Intro/intro.js
@@ -0,0 +1,9 @@
+//output anything on console
+console.log("hello world!!");
+//top to down
+//left to right
+
+//javascript datatypes: Number(int, double, float, big int), Boolean, String('a', "asfdasdf"), undefined, Object()
+// in java c++: DataType varibaleName = value;
+// in JS we use let or const nd both are block scoped variables
+
