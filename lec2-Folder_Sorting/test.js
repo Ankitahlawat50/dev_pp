bba953e1a66f8ea35bee5e621d205140c2c6536f
Ankit Ahlawat <ankitahlawat.50@gamil.com>
2021-08-23 01:11:28 +0530

    final commit

--- lec2-Folder_Sorting/test.js
+++ lec2-Folder_Sorting/test.js
@@ -1,11 +1,35 @@
-let a=10;
-console.log(a);
-function change2(){
-    console.log(a);
+// let a=10;
+// console.log(a);
+// function change2(){
+//     console.log(a);
+// }
+// function change(){
+//     a=20;
+//     change2();
+// }
+// change();
+// console.log(a);
+
+let b='folder';
+
+function change3(){
+    console.log(b);
 }
-function change(){
-    a=20;
-    change2();
+function change3x(b){
+    console.log(b);
 }
-change();
-console.log(a);
+let i=0;
+function change4(b){
+    i++;
+    if(i>1){
+
+    }else{
+        b=`${b}/abc`;
+        change4(b);
+    }
+    
+    //change3();
+    change3x(b)
+}
+change4(b);
+console.log(b);
