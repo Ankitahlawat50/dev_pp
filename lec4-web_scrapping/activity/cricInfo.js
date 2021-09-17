f96eba44f5f68b7c224771c82856659c61cd81eb
Ankit Ahlawat <ankitahlawat.50@gamil.com>
2021-09-18 02:07:21 +0530

    webScrapping

---
+++ lec4-web_scrapping/activity/cricInfo.js
@@ -0,0 +1,34 @@
+const request= require("request");
+const cheerio= require("cheerio");
+//const { last } = require("cheerio/lib/api/traversing");
+let lastComm="https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/ball-by-ball-commentary";
+let mostWicket="https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
+
+request(lastComm, function cb(error,response,body){
+    parseDatacomm(body);
+});
+request(mostWicket,function(error,response,body){
+    parseDataWicket(body);
+});
+function parseDatacomm(html){
+    let ch=cheerio.load(html);
+    let comm=ch('.match-comment .d-flex.match-comment-padder.align-items-center .match-comment-wrapper .match-comment-long-text p');
+    let c=ch(comm[0]).text();
+    console.log(c);
+
+}
+
+function parseDataWicket(html){
+    let ch =cheerio.load(html);
+    let wt=ch('.table.bowler tbody tr .cursor-pointer.bowler-wicket');
+    let max=0;
+    for(let i=0;i<wt.length;i++){
+        let t=ch(wt[0]).text();
+        if(t>max){
+            max=t;
+        }
+    }
+    console.log(max);
+}
+
+
