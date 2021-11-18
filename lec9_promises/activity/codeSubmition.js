219e77143091f26e21607f5045f568ebca251670
Ankit Ahlawat <ankitahlawat.50@gamil.com>
2021-11-18 23:25:37 +0530

    login complete

--- lec9_promises/activity/codeSubmition.js
+++ lec9_promises/activity/codeSubmition.js
@@ -1,22 +1,40 @@
 let puppeteer=require("puppeteer");
+const id="vadafa5095@erpipo.com";
+const pw="123456789";
+let tab;
 //all all funtions of pupeeteer promisified => gives you a pending promise initially
 
-let browserOpenPromise=puppeteer.launch({headless: false});
+let browserOpenPromise=puppeteer.launch({headless: false, defaultViewport:null, args: ["--start-maximized"], slowMo: 100});
 
 browserOpenPromise.then(function(browser){
     console.log("browser opened");
-    console.log(browser);
+    //console.log(browser);
     let allPagesOpened=browser.pages();
     return allPagesOpened
 })
 .then(function(pages){
-    let tab=pages[0];
-    let PageOnePromised=tab.goto("https://www.google.com");
+    tab=pages[0];
+    let PageOnePromised=tab.goto("https://www.hackerrank.com/auth/login");
     return PageOnePromised;
 })
 .then(function(data){
-    console.log("google homepage opened");
+   let idTypePromise=tab.type("#input-1",id);
+   return idTypePromise
 })
+.then(function(data){
+    let pwTypePromise=tab.type("#input-2",pw);
+    return pwTypePromise
+})
+.then(function(){
+    console.log("id and pw typed");
+    let loginPromise=tab.click('.ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled');
+    return loginPromise;
+})
+.then(function(){
+
+})
+
+
 
 
 // const puppeteer = require('puppeteer');
